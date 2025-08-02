60738990d52906bc902da821005009d0cf44adb8
its-me-abhiram <abhiram143sag@gmail.com>
2025-08-02 22:55:29 +0530

    bug-fixing with footer

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ def index(request):
     city = request.GET.get('city', 'bangalore')
     api_key = '03b1acbfd0387ac994dad57eb102fe01'
     api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
-    # print(api_url)
+
   
 
     api = requests.get(api_url).json()
